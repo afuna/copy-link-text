c44dab82b8605835a2fe9f296ba027fca1019ec5
Andrea Nall <anall@andreanall.com>
2010-09-30 20:57:11 -0600

    Handle the case Afuna mentioned.

--- menu.js
+++ menu.js
@@ -1,7 +1,14 @@
 document.addEventListener( "contextmenu", function(event) {
-    if ( event.target.nodeName === "A" ) {
-        chrome.extension.sendRequest({
-            "text": event.target.innerText
-        });
+    var node = event.target;
+    var limit = 5; // Try and not hang the browser for a really complex dom tree -- hopefully we don't have stuff THIS nested
+    while ( node && ( limit-- ) > 0 ) {
+        if ( node.nodeName === "A" && node.href != undefined ) {
+            chrome.extension.sendRequest({
+                "text": node.innerText
+            });
+            return true;
+        } else {
+            node = node.parentNode;
+        }
     }
 }, false );
