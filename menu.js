4c84ecdd1cfda872c3b94796d1fde90011650068
Afuna <coder.github@afunamatata.com>
2010-10-01 11:23:57 +0800

    Add a couple of comments.

--- menu.js
+++ menu.js
@@ -1,7 +1,12 @@
+// listener is on the document so that we also handle links added after page load
 document.addEventListener( "contextmenu", function(event) {
     var node = event.target;
-    var limit = 5; // Try and not hang the browser for a really complex dom tree -- hopefully we don't have stuff THIS nested
-    while ( node && ( limit-- ) > 0 ) {
+
+    // handle nested tags within the link
+    // let's try and not hang the browser for a really complex dom tree 
+    // -- hopefully we don't have stuff THIS nested
+    var limit = 5;
+    while ( node && ( limit-- > 0 ) ) {
         if ( node.nodeName === "A" && node.href != undefined ) {
             chrome.extension.sendRequest({
                 "text": node.innerText
